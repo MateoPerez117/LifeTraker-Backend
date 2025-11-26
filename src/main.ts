1025db4df62585f04a6bd16fbabbdb85a24e0bc8
OmarAguilar43 <omaroaguilar43@gmail.com>
2025-11-25 23:01:26 -0600

    resolucion de error main

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist:true,
-      forbidNonWhitelisted:true,
-      transform:true
-    })
-  )
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
+  const config = new DocumentBuilder()
+    .setTitle('LifeTraker API')
+    .setDescription('Documentación de la API LifeTraker')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
-
-  
 }
-bootstrap();
+
+// 👉 ESTA ES LA PARTE CLAVE
+bootstrap().catch((err) => {
+  console.error('Error al iniciar:', err);
+});
